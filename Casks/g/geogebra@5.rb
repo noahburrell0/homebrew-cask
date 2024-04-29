d97aa6c81e537d52080e3a6dfa2d55aca855db85
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-04-29 17:03:35 -0400

    geogebra@5: migrate

---
+++ Casks/g/geogebra@5.rb
@@ -0,0 +1,27 @@
+cask "geogebra@5" do
+  version "5.2.838.0"
+  sha256 "ae3e93e0595cb00d97c779c6fba95f7a0f17fcee907c8e786cb3967413a6cceb"
+
+  url "https://download.geogebra.org/installers/#{version.major_minor}/GeoGebra-MacOS-Installer-withJava-#{version.dots_to_hyphens}.zip"
+  name "GeoGebra"
+  desc "Solve, save and share math problems, graph functions, etc"
+  homepage "https://www.geogebra.org/"
+
+  livecheck do
+    url "https://download.geogebra.org/package/mac"
+    regex(%r{/GeoGebra[._-]MacOS[._-]Installer[._-]withJava[._-]v?(\d+(?:-\d+)+)\.zip}i)
+    strategy :header_match do |headers, regex|
+      match = headers["location"][regex, 1]
+      next if match.blank?
+
+      match.tr("-", ".")
+    end
+  end
+
+  app "Geogebra.app"
+
+  uninstall quit:       "org.geogebra#{version.major}.mac",
+            login_item: "Geogebra"
+
+  zap trash: "~/Library/Saved Application State/org.geogebra5.mac.savedState"
+end
