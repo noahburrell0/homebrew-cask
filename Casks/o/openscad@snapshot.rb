6743c0fa0190d00f85e7ef4d5a6d9a8b0f3a4986
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-04-30 09:16:51 -0400

    openscad@snapshot: migrate

---
+++ Casks/o/openscad@snapshot.rb
@@ -0,0 +1,25 @@
+cask "openscad@snapshot" do
+  version "2024.04.26"
+  sha256 "fca7662741073298a0679e68bd0ac58fec731b8becf853deda0185b302e0c3ac"
+
+  url "https://files.openscad.org/snapshots/OpenSCAD-#{version}.dmg"
+  name "OpenSCAD"
+  desc "Programmable solid 3D CAD modeller"
+  homepage "https://www.openscad.org/downloads.html#snapshots"
+
+  livecheck do
+    url "https://files.openscad.org/snapshots/.snapshot_macos.js"
+    regex(/OpenSCAD[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
+  end
+
+  conflicts_with cask: "openscad"
+
+  app "OpenSCAD.app"
+  binary "#{appdir}/OpenSCAD.app/Contents/MacOS/OpenSCAD", target: "openscad"
+
+  zap trash: [
+    "~/Library/Caches/org.openscad.OpenSCAD",
+    "~/Library/Preferences/org.openscad.OpenSCAD.plist",
+    "~/Library/Saved Application State/org.openscad.OpenSCAD.savedState",
+  ]
+end
